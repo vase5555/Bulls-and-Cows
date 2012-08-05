303c5a7a9d7b54e09100f61b513300122c4438af
Vasil Hristov <vase5555@gmail.com>
2012-08-05 14:31:23 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,14 @@ using System.Text;
 
 namespace Bows_And_Cows
 {
+    /// <summary>
+    /// Falling rock game made with list and struct
+    /// </summary>
+    /// 
+    /// <copyright>
+    /// (c) Vasil Hristov, 2012 - http://www.vhristov.com
+    /// </copyright>
+    /// 
     class Program
     {
         static void Main(string[] args)
